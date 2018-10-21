be5d870d1c8867825091e939bf8a201271a291ce
groscoe <gustavo.roscoe@waycarbon.com>
2018-10-21 13:26:14 -0300

    Added a fitness proportional selection function.

--- src/Algorithm/Evolutionary/Operators/Selection.hs
+++ src/Algorithm/Evolutionary/Operators/Selection.hs
@@ -1,18 +1,24 @@
 module Algorithm.Evolutionary.Operators.Selection (
   shuffleAndSelect,
-  dropWeakest
+  dropWeakest,
+  fitnessProportionalByRoulette
   ) where
 
 import Control.Monad.Random.Class
+import Control.Monad (replicateM)
 import System.Random.Shuffle (shuffleM)
 import Data.List (sortOn)
 
 import Algorithm.Evolutionary.Internals.Population
 
 shuffleAndSelect :: (MonadRandom m, Real n) => (ind -> n) -> Int -> Population ind -> m [ind]
-shuffleAndSelect fitnessFunction numParents =
-  fmap (take numParents . sortOn fitnessFunction . take (2 * numParents)) . shuffleM . getPopulation
+shuffleAndSelect fitnessFunction λ =
+  fmap (take λ . sortOn fitnessFunction . take (2 * λ)) . shuffleM . getPopulation
 
 dropWeakest :: (Applicative m, Real n) => (ind -> n) -> Int -> Population ind -> m [ind]
 dropWeakest fitness numToDrop =
   pure . drop numToDrop . sortOn (negate . fitness) . getPopulation
+
+fitnessProportionalByRoulette :: (MonadRandom m, Real n) => (ind -> n) -> Int -> Population ind -> m [ind]
+fitnessProportionalByRoulette fitness λ population = replicateM λ . weighted $
+  (\candidate -> (candidate, toRational $ fitness candidate)) <$> getPopulation population
